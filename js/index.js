799e1725625259fbeef024a981e4eded7ebed5c5
ChyoRi <73056194+ChyoRi@users.noreply.github.com>
2022-09-02 11:36:53 +0900

    수정본

--- js/index.js
+++ js/index.js
@@ -1,80 +1,83 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  /* 메인 스와이퍼*/
 
-    /* 메인 스와이퍼*/
+  const swiperMain = new Swiper(".main_slide_frame", {
+    pagination: {
+      el: ".main_slide_nav",
+      type: "bullets",
+    },
+    slidesPerView: 1.9,
+    spaceBetween: 85,
+  });
 
-    const swiperMain = new Swiper(".main_slide_frame", {
-        pagination : {
-            el: '.main_slide_nav',
-            type: 'bullets'
-        },
-        slidesPerView: 1.9,
-        spaceBetween: 85
-    });
+  /* 탭 메뉴 버튼 스와이퍼 */
 
-    /* 탭 메뉴 버튼 스와이퍼 */
+  const swiperTabBtn = new Swiper(".tab_btn_frame", {
+    slidesPerView: 4.8,
+    spaceBetween: 20,
+    freeMode: true,
+  });
 
-    const swiperTabBtn = new Swiper(".tab_btn_frame", {
-        slidesPerView: 4.8,
-        spaceBetween: 20,
-        freeMode : true
-    });
+  /* 탭 메뉴 내용 슬라이드 스와이퍼 */
 
-    /* 탭 메뉴 내용 슬라이드 스와이퍼 */
+  const swiperTabCont = new Swiper(".tab_cont_slide_frame", {
+    pagination: {
+      el: ".tab_cont_slide_nav",
+      type: "bullets",
+    },
+    slidesPerView: 1.9,
+    freeMode: true,
+  });
 
-    const swiperTabCont = new Swiper(".tab_cont_slide_frame", {
-        pagination : {
-            el: '.tab_cont_slide_nav',
-            type: 'bullets'
-        },
-        slidesPerView: 1.9,
-        freeMode : true
-    });
+  /* 전역 변수 */
 
-    /* 전역 변수 */
+  const footerAccordionBtn = $(".footer_btn");
 
-    const footerAccordionBtn = $('.footer_btn');
-    
-    /* 헤더 배경색 변경 함수 */
+  /* 헤더 배경색 변경 함수 */
 
-    const headerFixed = () => {
-        const header = $('header');
-        let headerHeight = $('header').outerHeight();
-        let windowTop = $(document).scrollTop();
+  const headerFixed = () => {
+    const header = $("header");
+    let headerHeight = $("header").outerHeight();
+    let windowTop = $(document).scrollTop();
 
-        if (headerHeight <= windowTop) {
-            header.css({'background-color' : '#eb605f'});
-        } else {
-            header.css({'background-color' : 'transparent'});
-        }
+    if (headerHeight <= windowTop) {
+      header.css({ "background-color": "#eb605f" });
+    } else {
+      header.css({ "background-color": "transparent" });
     }
+  };
 
-    /* 푸터 아코디언 함수 */
-    
-    const accordion = () => {
-        const footerAccordionWrap = $('.info_accordion_wrap');
+  /* 푸터 아코디언 함수 */
 
-        footerAccordionBtn.toggleClass('active');
-        footerAccordionWrap.stop().slideToggle();
-    }
+  const accordion = () => {
+    const footerAccordionWrap = $(".info_accordion_wrap");
 
-    /* 탭 메뉴 함수 */
-    
-    const tabBtn = $('.tab_btn_frame > .tab_btn_ul > div > a');
-    console.log(tabBtn);
-    const tabCont = $('.tab_cont_frame > ul > li');
+    footerAccordionBtn.toggleClass("active");
+    footerAccordionWrap.stop().slideToggle();
+  };
 
-    const tabBtnFn = (e) => {
-        e.preventDefault();
-        $(this).toggleClass('active');
-        let i = $(this).index();
-        console.log(i);
-        tabCont.eq(i).addClass('active').siblings().removeClass('active');
-    }
+  /* 탭 메뉴 함수 */
 
+  const tabBtn = $(".tab_btn_frame > .tab_btn_ul > div > a");
+  console.log(tabBtn);
+  const tabCont = $(".tab_cont_frame > ul > li");
 
-    footerAccordionBtn.click(accordion);
-    $(document).scroll(headerFixed);
+  const tabBtnFn = (e) => {
+    e.preventDefault();
+    const thisEl = $(e?.currentTarget);
+    const i = thisEl?.parent()?.index();
 
-    tabBtn.click(tabBtnFn);
+    thisEl
+      ?.addClass("active")
+      ?.parent()
+      ?.siblings()
+      ?.children("a")
+      ?.removeClass("active");
+    tabCont?.eq(i)?.addClass("active")?.siblings()?.removeClass("active");
+  };
 
+  footerAccordionBtn.click(accordion);
+  $(document).scroll(headerFixed);
+
+  tabBtn.click(tabBtnFn);
 });
